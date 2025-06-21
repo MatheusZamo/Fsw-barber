eb41bff44768caf921a976d192ff5ea93b6e8f98
MatheusZamo <19matheushenrique03@gmail.com>
2025-06-20 23:07:26 -0300

    feat: ensure only authenticated user can book

--- src/app/actions/create-booking.ts
+++ src/app/actions/create-booking.ts
@@ -1,6 +1,8 @@
 "use server"
 
+import { authOptions } from "@/lib/auth"
 import { db } from "@/lib/prisma"
+import { getServerSession } from "next-auth"
 import { revalidatePath } from "next/cache"
 
 interface CreateBookingParams {
@@ -10,6 +12,10 @@ interface CreateBookingParams {
 }
 
 export const createBooking = async (params: CreateBookingParams) => {
+  const user = await getServerSession(authOptions)
+  if (!user) {
+    throw new Error("Usuário não autenticado")
+  }
   await db.booking.create({
     data: params,
   })
