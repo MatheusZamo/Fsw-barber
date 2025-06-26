588f055afa78ad29ee0b1d962074b4a13ae89ca0
MatheusZamo <19matheushenrique03@gmail.com>
2025-06-26 07:00:53 -0300

    refactor: reorganize layout and improve component structure in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,66 +29,66 @@ const Home = async () => {
     <div>
       <Header />
       <div className="mx-auto max-w-5xl p-5 lg:flex lg:w-full lg:flex-col lg:justify-center">
-        <div>
-          <h2 className="text-xl font-bold">
-            Olá,{" "}
-            {session?.user ? session.user.name?.split(" ")[0] : "Bem Vindo"}!
-          </h2>
-          <p>
-            <span className="capitalize">
-              {format(new Date(), "EEEE, dd", { locale: ptBR })}
-            </span>
-            <span>&nbsp;de&nbsp;</span>
-            <span className="capitalize">
-              {format(new Date(), "MMMM", { locale: ptBR })}
-            </span>
-          </p>
-
-          <div className="mt-6 max-w-xl">
-            <Search />
+        <div className="lg:flex lg:justify-between">
+          <div className="lg:pt-5">
+            <h2 className="text-xl font-bold">
+              Olá,{" "}
+              {session?.user ? session.user.name?.split(" ")[0] : "Bem Vindo"}!
+            </h2>
+            <p>
+              <span className="capitalize">
+                {format(new Date(), "EEEE, dd", { locale: ptBR })}
+              </span>
+              <span>&nbsp;de&nbsp;</span>
+              <span className="capitalize">
+                {format(new Date(), "MMMM", { locale: ptBR })}
+              </span>
+            </p>
+            <div className="mt-6 lg:w-[350px]">
+              <Search />
+            </div>
           </div>
-        </div>
-
-        <div className="mt-6 flex gap-3 overflow-x-scroll lg:hidden [&::-webkit-scrollbar]:hidden">
-          {quickSearchOptions.map((option) => (
-            <Button
-              className="gap-2"
-              variant="secondary"
-              key={option.title}
-              asChild
-            >
-              <Link href={`barbershops?service=${option.title}`}>
-                <Image
-                  src={option.imageUrl}
-                  width={16}
-                  height={16}
-                  alt={option.title}
-                />
-                {option.title}
-              </Link>
-            </Button>
-          ))}
-        </div>
-
-        <div className="relative mt-6 h-[150px] w-full lg:hidden">
-          <Image
-            alt="Agende nas melhores com FSW Barber"
-            src="Banner.svg"
-            fill
-            className="rounded-xl object-cover"
-          />
-        </div>
-
-        {confirmedBookings.length > 0 && (
-          <Carousel title="Agendamentos" className="mt-6">
-            {confirmedBookings.map((booking) => (
-              <BookingItem
-                key={booking.id}
-                booking={JSON.parse(JSON.stringify(booking))}
-              />
+          <div className="mt-6 flex gap-3 overflow-x-scroll lg:hidden [&::-webkit-scrollbar]:hidden">
+            {quickSearchOptions.map((option) => (
+              <Button
+                className="gap-2"
+                variant="secondary"
+                key={option.title}
+                asChild
+              >
+                <Link href={`barbershops?service=${option.title}`}>
+                  <Image
+                    src={option.imageUrl}
+                    width={16}
+                    height={16}
+                    alt={option.title}
+                  />
+                  {option.title}
+                </Link>
+              </Button>
             ))}
-          </Carousel>
-        )}
+          </div>
+
+          <div className="relative mt-6 h-[150px] w-full lg:hidden">
+            <Image
+              alt="Agende nas melhores com FSW Barber"
+              src="Banner.svg"
+              fill
+              className="rounded-xl object-cover"
+            />
+          </div>
+
+          {confirmedBookings.length > 0 && (
+            <Carousel title="Agendamentos" className="mt-6 lg:w-[380px]">
+              {confirmedBookings.map((booking) => (
+                <BookingItem
+                  key={booking.id}
+                  booking={JSON.parse(JSON.stringify(booking))}
+                />
+              ))}
+            </Carousel>
+          )}
+        </div>
 
         <Carousel title="Recomendados" className="mt-6">
           {barbershops.map((barbershop) => (
